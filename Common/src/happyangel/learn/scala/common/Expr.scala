07255b5a19e136e9e95e7d4eaa9576d72a2fbb9b
xionglei <HappyAngel880503@gmail.com>
2017-06-25 22:46:23 +0800

    add code for pattern matching

--- Common/src/happyangel/learn/scala/common/Expr.scala
+++ Common/src/happyangel/learn/scala/common/Expr.scala
@@ -1,16 +1,47 @@
 package happyangel.learn.scala.common
 
+import happyangel.learn.scala.common.Element
 /**
   * Created by xionglei on 17-6-23.
   *
   * from The scala programming language ch15
   *
   */
-abstract class Expr
+sealed abstract class Expr
 
 case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
 
+class ExprFormatter {
+    // Contains operators in groups of increasing precedence
+    private val opGroups =
+        Array(
+            Set("|", "||"),
+            Set("&", "&&"),
+            Set("^"),
+            Set("==", "!="),
+            Set("<", "<=", ">", ">="),
+            Set("+", "-"),
+            Set("*", "%")
+        )
+
+    private val precedence = {
+        val assocs =
+            for {
+                i <- 0 until opGroups.length
+                op <- opGroups(i)
+            } yield op -> i
+        assocs.toMap
+    }
+
+    // unary precedence is larger than binary
+    private val unaryPrecedence = opGroups.length
+    private val fractionPrecedence = -1
+
+    private def format(e: Expr, enclPrec: Int): Element = e match {
+
+    }
+}
 
