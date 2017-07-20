05f6cc5db66d0d25dfd4e317b8fede83bfac471b
xionglei <xionglei@xiaomi.com>
2017-07-20 21:30:07 +0800

    add code to currency

--- Common/src/main/scala/happyangel/learn/scala/common/Currency.scala
+++ Common/src/main/scala/happyangel/learn/scala/common/Currency.scala
@@ -5,13 +5,89 @@ package happyangel.learn.scala.common
   *
   *  to demonstrate The Scala programming language ch20
   */
-abstract class Currency {
-  val amount: Long
-  def designation: String
+abstract class CurrencyZone {
+    type Currency <: AbstractCurrency
+    def make(amount: Long): Currency
 
-  override def toString: String = amount + " " + designation
+    abstract class AbstractCurrency {
+        val amount: Long
 
-  def + (that: Currency): Currency = {
-    amount + that.amount
-  }
+        def designation: String
+
+        def +(that: Currency): Currency = {
+            make(amount + that.amount)
+        }
+
+        def *(x: Double): Currency = {
+            make((amount * x).toLong)
+        }
+
+        def -(that: Currency): Currency = {
+            make(this.amount - that.amount)
+        }
+
+        def / (that: Double) = {
+            make((this.amount / that).toLong)
+        }
+
+        def / (that: Currency) = {
+            this.amount.toDouble / that.amount
+        }
+
+        def from(other: CurrencyZone#AbstractCurrency): Currency = {
+            make(math.round(other.amount.toDouble * Converter.exchangeRate(other.designation)(this.designation)))
+        }
+
+        override def toString =
+            ((amount.toDouble / CurrencyUnit.amount.toDouble) formatted("%." + decimals(CurrencyUnit.amount) + "f") + " " + designation)
+
+        private def decimals(n: Long): Int = {
+            if (n==1) 0 else 1 + decimals(n/10)
+        }
+    }
+
+    val CurrencyUnit: Currency
 }
+
+object US extends CurrencyZone {
+    type Currency = Dollar
+    abstract class Dollar extends AbstractCurrency {
+        type Currency = Dollar
+        def designation = "USD"
+    }
+
+    override def make(x: Long) = new Dollar {
+        override val amount: Long = x
+    }
+
+    val Cent = make(1)
+    val Dollar = make(100)
+    val CurrencyUnit = Dollar
+}
+
+object Europe extends CurrencyZone {
+    abstract class Euro extends AbstractCurrency {
+        override def designation = "EUR"
+    }
+
+    override type Currency = Euro
+    def make(cents: Long) = new Euro {
+        val amount = cents
+    }
+
+    val Cent = make(1)
+    val Euro = make(100)
+    val CurrencyUnit = Euro
+}
+
+object Converter {
+    var exchangeRate = Map (
+        "USD" -> Map("USD" -> 1.0, "EUR" -> 0.756, "JPY" -> 1.211, "CHY" -> 1.233),
+        "EUR" -> Map("EUR" -> 1.0, "USD" -> 1.316)
+    )
+}
+
+object test extends App {
+    println(US.Dollar from Europe.Euro)
+}
+
