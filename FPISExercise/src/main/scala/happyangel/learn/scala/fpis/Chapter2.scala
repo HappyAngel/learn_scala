72c5219992a08739df9659a9d7cb2acd37860b65
xionglei <HappyAngel880503@gmail.com>
2018-04-15 23:07:30 +0800

    finish chapter2 problem

--- FPISExercise/src/main/scala/happyangel/learn/scala/fpis/Chapter2.scala
+++ FPISExercise/src/main/scala/happyangel/learn/scala/fpis/Chapter2.scala
@@ -1,10 +1,13 @@
 package happyangel.learn.scala.fpis
 
+import scala.annotation.Annotation
+
 /**
   * Created by xionglei on 18-4-12.
   */
 object Chapter2 extends App {
     def fib(n: Int): Int = {
+        @annotation.tailrec
         def tmp(prev1: Int, prev0: Int, n: Int): Int = {
             if (n == 3)
                 prev0 + prev1
@@ -21,6 +24,28 @@ object Chapter2 extends App {
             tmp(0, 1, n)
     }
 
+    def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+        @annotation.tailrec
+        def test(n: Int): Boolean = {
+            if (n >= as.length - 1) true
+            else if (!ordered(as(n), as(n+1))) false
+            else test(n+1)
+        }
 
-    println(fib(5))
+        test(0)
+    }
+
+    def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+       a => b => f(a,b)
+    }
+
+    def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+        (a, b) => f(a)(b)
+    }
+
+    def compose[A,B,C](f: B => C, g: A => B): A => C = {
+        a => f(g(a))
+    }
+
+    println(curry((a: Int,b: Int) => a > b)(1)(2))
 }
