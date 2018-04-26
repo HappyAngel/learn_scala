d1b83ffd2d422b1823246a0763def932cb9d5e31
xionglei <xionglei@xiaomi.com>
2018-04-26 22:05:18 +0800

    init chapter3 exer

--- project/LearnScala.scala
+++ project/LearnScala.scala
@@ -9,7 +9,12 @@ object LearnScala extends Build {
                         "com.google.code.gson" % "gson" % "2.3.1",
                         "commons-codec" % "commons-codec" % "1.10",
                         "com.typesafe.play" %% "play-json" % "2.5.4",
-                        "net.sf.json-lib" % "json-lib" % "2.2.3"
+                        "net.sf.json-lib" % "json-lib" % "2.2.3",
+                        "com.google.guava" % "guava" % "20.0",
+                        "com.xiaomi" %% "ai-api-common" % "1.1.37",
+                        "com.xiaomi" % "ai-api-spec_2.11" % "1.0.27",
+                        "ai.x" %% "play-json-extensions" % "0.10.0"
+
                     )
                 )
 
