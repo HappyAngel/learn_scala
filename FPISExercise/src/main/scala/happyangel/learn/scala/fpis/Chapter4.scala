2d91d00e21ef1f03ba184f140c4b8cabe04c77e6
xionglei <HappyAngel880503@gmail.com>
2018-05-15 23:24:12 +0800

    initial change for chapter 4

--- FPISExercise/src/main/scala/happyangel/learn/scala/fpis/Chapter4.scala
+++ FPISExercise/src/main/scala/happyangel/learn/scala/fpis/Chapter4.scala
@@ -4,9 +4,54 @@ package fpis
   * Created by happyangel on 2018/5/13.
   */
 
-sealed trait Option[+A]
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
+sealed trait MyOption[+A] {
+  def map[B](f: A => B): MyOption[B] = {
+    this match {
+      case MySome(v) =>
+        MySome(f(v))
+      case MyNone =>
+        MyNone
+    }
+  }
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] = {
+    this match {
+      case MySome(v) =>
+        f(v)
+      case MyNone =>
+        MyNone
+    }
+  }
+  def getOrElse[B >: A](default: => B): B = {
+    this match {
+      case MySome(v) =>
+        v
+      case MyNone =>
+        default
+    }
+  }
+
+  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = {
+    this match {
+      case MySome(v) =>
+        this
+      case MyNone =>
+        ob
+    }
+  }
+  def filter(f: A => Boolean): MyOption[A] = {
+    this match {
+      case MySome(v) =>
+        if (f(v))
+          this
+        else
+          MyNone
+      case MyNone =>
+        MyNone
+    }
+  }
+}
+case class MySome[+A](get: A) extends MyOption[A]
+case object MyNone extends MyOption[Nothing]
 
 object Chapter4 {
 
